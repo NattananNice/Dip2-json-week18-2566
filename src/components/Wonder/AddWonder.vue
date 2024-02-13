2971de09c199c8438b55002929423522b3eee2e0
phisit77 <kachidoki787>
2024-02-13 15:43:10 +0700

    update

---
+++ src/components/Wonder/AddWonder.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+
+            <div class="col-md-5 mb-1 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header p-0">
+                        <img src="https://f.ptcdn.info/768/044/000/ob199a9417NbQ6z44lv-o.jpg" class="card-img-top" alt="wonders">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header " style="background: #eb7ce5">
+                        <span class="text-white fs-5">
+                            ลงทะเบียนสถานที่เที่ยวในไทย
+                        </span>
+                    </div>
+                    <div class="card-body text-success">
+                        <form @submit.prevent="addWonder">
+                            <div class="mb-1">
+                                <label for="">ชื่อสิ่งสถานที่เที่ยวในไทย</label>
+                                <input type="text" class="form-control" v-model="wonderState.name" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ตำแหน่ง ที่อยู่ สถานที่เที่ยวในไทย</label>
+                                <input type="text" class="form-control" v-model="wonderState.label" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ลิงค์ภาพสถานที่เที่ยวในไทยก</label>
+                                <input type="text" class="form-control" v-model="wonderState.imageUrl" />
+                            </div>
+
+                            <div class="mb-1">
+
+                                <button type="submit" class=" text-white shadow" style="background: #eb7ce5">
+                                    ลงทะเบียนสถานที่เที่ยวในไทย
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const wonderState = reactive({
+    name: '',
+    location: '',
+    imageUrl:''
+})
+
+const router = useRouter()
+
+const addWonder = async () => {
+    await axios.post(`https://json-server-vue3-d109.onrender.com/wonders`,wonderState)
+        .then(() => {
+            console.log(wonderState)
+            router.push({name:'wonders'})
+        })
+    alert('ลงทะเบียนสถานที่ท่องเที่ยวในไทย เรียบร้อยแล้ว')
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.container {
+    font-family: 'Sarabun', sans-serif;
+
+}
+
+.form-label {
+    color: green;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+toast.info("ยินดีต้อนรับแผนกเทคโนโลยีสารสนเทศ", {
+  autoClose: 1500,
+  position:toast.POSITION.TOP_CENTER
+})
+
+const notify = () => {
+  toast.success("ยินดีต้อนรับแผนกเทคโนโลยีสารสนเทศ", {
+    autoClose: 1500,
+    position: toast.POSITION.TOP_CENTER
+  })
+  return notify
+}
+
+</script>
+
+<template>
+  <div class="container my-5">
+    <div class="card shadow" style="width: 20rem;">
+      <div class="card-header p-0">
+        <img src="https://image.makewebcdn.com/makeweb/m_1920x0/oDgJmY2kJ/DefaultData/10_%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%97%E0%B9%88%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B9%83%E0%B8%99%E0%B9%84%E0%B8%97%E0%B8%A2%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%95%E0%B8%B4%E0%B8%94%E0%B8%AD%E0%B8%B1%E0%B8%99%E0%B8%94%E0%B8%B1%E0%B8%9A%E0%B9%82%E0%B8%A5%E0%B8%81_copy.jpg" class="card-img-top"
+          alt="img">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">สถานที่ท่องเที่ยว</h5>
+        <p class="card-text"> ใน ประเทศไทย บ้านเรามีที่เที่ยวเยอะแยะแบบที่ไปกันได้ทั้งปีทุกวี่ทุกวันก็ยังไม่มีหมดค่ะ นี่ยังไม่นับรวมแหล่งท่องเที่ยวใหม่ๆ ที่ผุดกันขึ้นมาเรื่อยๆ เรียกได้ว่าเมืองไทยของเรานั้น แทบจะเป็นสวรรค์ของนักท่องเที่ยวกันเลย</p>
+        <a href="#" class="btn btn-primary" @click="notify" style="background: #eb7ce5">Read...</a>
+      </div>
+    </div>
+  </div>
+</template>
